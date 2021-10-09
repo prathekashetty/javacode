2fc3dad8c5270841e86b823af263fdbdaa2b3311
prathekashetty <92204370+prathekashetty@users.noreply.github.com>
2021-10-09 01:15:59 -0700

    Add files via upload

---
+++ variable/Phone.java
@@ -0,0 +1,26 @@
+class Phone{
+	static float price;
+	static int model_id;
+    static byte num;
+	static short num1;
+	static long num2;
+	static double num3;
+	static char num4;
+	static boolean num5;
+    
+
+
+public static void main(String []args)
+{
+  //  int model_id=123;
+   System.out.println(model_id);
+   System.out.println(price);
+   System.out.println(num);
+   System.out.println(num1);
+   System.out.println(num2);
+   System.out.println(num3);
+   System.out.println(num4);
+   System.out.println(num5);
+
+}
+}
