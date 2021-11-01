78a73885202a461dd31d9c86744f81df4dd09404
prathekashetty <92204370+prathekashetty@users.noreply.github.com>
2021-11-01 08:45:58 -0700

    Add files via upload

---
+++ HTML-CSS-JS/cooldrinks/src/main/java/com/wolken/cooldrinks/LimcaServlet.java
@@ -0,0 +1,22 @@
+package com.wolken.cooldrinks;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LimcaServlet  extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher=req.getRequestDispatcher("Limca.html");
+        dispatcher.include(req,resp);
+
+}
+    
+
+
+}
